3667ce779940ddc102b56d09d4b1911277ef3702
noothanks <coreyfuller727@yahoo.com>
2022-04-28 18:48:31 -0400

    adding high score feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -130,6 +130,29 @@ var startGame = function() {
 // function to end the entire game
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
+//comparing new score to high score
+  var highscore = localStorage.getItem("highscore");
+    if (highscore === null) {
+      highscore = 0;
+    }
+//if new highscore
+    if (playerInfo.money > highscore) {
+      localStorage.setItem("highscore", playerInfo.money);
+      localStorage.setItem("name", playerInfo.name)
+
+      alert(`${playerInfo.name} now has a high score of ${playerInfo.money}!`);
+    }
+    else {
+      alert(`${playerInfo.name} did not beat the high score of ${highscore}. Maybe next time!`);
+    }
+
+    // ask player if they'd like to play again
+  var playAgainConfirm = window.confirm('Would you like to play again?');
+
+  if (playAgainConfirm) {
+    startGame();
+  } else {
+    window.alert('Thank you for playing Robot Gladiators! Come back soon!');
 
   // if player is still alive, player wins!
   if (playerInfo.health > 0) {
@@ -138,13 +161,7 @@ var endGame = function() {
     window.alert("You've lost your robot in battle!");
   }
 
-  // ask player if they'd like to play again
-  var playAgainConfirm = window.confirm('Would you like to play again?');
-
-  if (playAgainConfirm) {
-    startGame();
-  } else {
-    window.alert('Thank you for playing Robot Gladiators! Come back soon!');
+  
   }
 };
 
